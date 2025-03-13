101b677f10697bf2ba40a38007ea4daeb2469528
Matheus Ferreira <158170071+Matheus-Ferreira-123@users.noreply.github.com>
2025-03-13 10:41:37 -0300

    post Exercicio2.py

---
+++ Exercicio2.py
@@ -0,0 +1,19 @@
+# Importando biblioteca
+from sklearn.linear_model import Perceptron
+
+# Dados de entrada
+X = [[0, 0], [0, 1], [1, 0], [1, 1]]
+
+# Saídas desejadas
+Y = [0, 1, 1, 1]
+
+# Criando e treinando o perceptron
+modelo = Perceptron()
+modelo.fit(X, Y)
+
+# Testando o modelo
+print("Previsões:")
+testes = [[0,0], [0,1], [1,0], [1,1]]
+for teste in testes:
+  previsao = modelo.predict([teste])
+  print(f"Ensolarado: {teste[0]}, Final de semana: {teste[1]},Parque lotado:{teste[1]} => ir ao parque? {'Sim' if previsao[0] == 1 else 'Não'}")
